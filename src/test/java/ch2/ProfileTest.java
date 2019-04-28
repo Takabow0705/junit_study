192d533a97125c862d7ff6e4208af705b8182948
takahiro haraguchi <hrgc.takahiro@outlook.jp>
2019-04-29 00:15:46 +0900

    [add] add second test in ProfileTest

--- src/test/java/ch2/ProfileTest.java
+++ src/test/java/ch2/ProfileTest.java
@@ -6,6 +6,7 @@ import org.junit.*;
 
 public class ProfileTest{
 
+    //必須の条件にマッチしない場合はFalseとなる。
     @Test
     public void matchAnswersFalseWhenMustMatchCriteriaNotMet(){
         Profile profile = new Profile("Bull Hockey, Inc.");
@@ -19,5 +20,20 @@ public class ProfileTest{
 
         boolean matches = profile.matches(criteria);
         assertFalse(matches);
+    }
+
+    @Test
+    public void matchAnswerTrueForAnyDontCareCriteria(){
+        Profile profile = new Profile("Bull Hockey,Inc.");
+        Question question = new BooleanQuestion(1, "牛乳は支給されますか");
+        Answer profileAnswer = new Answer(question,Bool.TRUE);
+        profile.add(profileAnswer);
+        Criteria criteria = new Criteria();
+        Answer criteriaAnswer = new Answer(question,Bool.TRUE);
+        Criterion criterion = new Criterion(criteriaAnswer, Weight.DontCare);
+        criteria.add(criterion);
+
+        boolean matches = profile.matches(criteria);
+        assertTrue(matches);
     }
 }
