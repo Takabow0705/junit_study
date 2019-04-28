12bc64639ef243ac8d78d18025cd37ba96d2fcd8
takahiro haraguchi <hrgc.takahiro@outlook.jp>
2019-04-28 11:44:52 +0900

    [add] add calc arithmeticMean test

--- src/test/java/ch1/ScoreCollectionTest.java
+++ src/test/java/ch1/ScoreCollectionTest.java
@@ -1,12 +1,29 @@
 package iloveyouboss;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 import org.junit.*;
+
+import iloveyouboss.ScoreCollection;
 
 public class ScoreCollectionTest{
 
     @Test
     public void test(){
-        fail("まだ実装されていません。");
+        //fail("まだ実装されていません。");
+    }
+
+    @Test
+    public void answersArthmeticMeanOfTwoNumbars(){
+        //Arrange
+        ScoreCollection collection = new ScoreCollection();
+        collection.add(() -> 7);
+        collection.add(() -> 5);
+
+        //Act
+        int actualResult = collection.arithmeticMean();
+
+        //Assert
+        assertThat(actualResult,equalTo(6));
     }
 }
