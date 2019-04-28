bd22e17673ceb75df6ed2a2929ad808a07633e16
takahiro haraguchi <hrgc.takahiro@outlook.jp>
2019-04-29 00:28:48 +0900

    [refactor] add create method and some comment

--- src/test/java/ch2/ProfileTest.java
+++ src/test/java/ch2/ProfileTest.java
@@ -5,15 +5,22 @@ import static org.hamcrest.CoreMatchers.*;
 import org.junit.*;
 
 public class ProfileTest{
+    private Profile profile;
+    private BooleanQuestion question;
+    private Criteria criteria;
+
+    @Before
+    public void create(){
+        profile = new Profile("Bull Hockey,Inc.");
+        question = new BooleanQuestion(1, "ボーナスは支給されますか？");
+        criteria = new Criteria();
+    }
 
     //必須の条件にマッチしない場合はFalseとなる。
     @Test
     public void matchAnswersFalseWhenMustMatchCriteriaNotMet(){
-        Profile profile = new Profile("Bull Hockey, Inc.");
-        Question question = new BooleanQuestion(1, "ボーナスは支給されますか？");
         Answer profileanswer = new Answer(question,Bool.FALSE);
         profile.add(profileanswer);
-        Criteria criteria = new Criteria();
         Answer criteriaAnswer = new Answer(question,Bool.TRUE);
         Criterion criterion = new Criterion(criteriaAnswer,Weight.MustMatch);
         criteria.add(criterion);
@@ -22,13 +29,11 @@ public class ProfileTest{
         assertFalse(matches);
     }
 
+    //不問の条件があればmatchesはTrueを返す。
     @Test
     public void matchAnswerTrueForAnyDontCareCriteria(){
-        Profile profile = new Profile("Bull Hockey,Inc.");
-        Question question = new BooleanQuestion(1, "牛乳は支給されますか");
-        Answer profileAnswer = new Answer(question,Bool.TRUE);
+        Answer profileAnswer = new Answer(question,Bool.FALSE);
         profile.add(profileAnswer);
-        Criteria criteria = new Criteria();
         Answer criteriaAnswer = new Answer(question,Bool.TRUE);
         Criterion criterion = new Criterion(criteriaAnswer, Weight.DontCare);
         criteria.add(criterion);
