d4b00c86f58049c9bd657660249c0c540c037bca
takahiro haraguchi <hrgc.takahiro@outlook.jp>
2019-06-08 09:22:30 +0900

    [revise] remove getter and setter to use lombok

--- src/main/java/dbaccess/dao/model/Bond.java
+++ src/main/java/dbaccess/dao/model/Bond.java
@@ -1,130 +1,45 @@
 package dbaccess.dao.model;
 
+import lombok.*;
+
 import java.math.BigDecimal;
 
+@Data
+@Builder
 public class Bond {
+    /* 債券コード**/
+    @Getter
+    @Setter
     private String code;
+    /* 債券名称**/
+    @Getter
+    @Setter
     private String name;
+    /* 債券の償還年限**/
+    @Getter
+    @Setter
     private String maturity;
+    /* クーポンレート**/
+    @Getter
+    @Setter
     private BigDecimal coupon;
+    /* 取得簿価**/
+    @Getter
+    @Setter
     private BigDecimal bookValue;
+    /* 時価**/
+    @Getter
+    @Setter
     private BigDecimal fairValue;
+    /* 債券数量**/
+    @Getter
+    @Setter
     private BigDecimal amount;
+    /* データの論理削除用フラグ**/
+    @Getter
+    @Setter
     private Boolean deleteFlag;
 
     private Bond(){}
-
-    public static Bond getInstance(){
-        return new Bond();
-    }
-
-    /**
-     * 
-     * @param code
-     * @return
-     */
-    public Bond setCode(String code){
-        this.code = code;
-        return this;
-    }
-    /**
-     * 
-     * @param name
-     * @return
-     */
-    public Bond setName(String name){
-        this.name = name;
-        return this;
-    }
-    /**
-    * 
-    * @param maturity
-    * @return
-    */
-    public Bond setMaturity(String maturity){
-        this.maturity = maturity;
-        return this;
-    }
-
-    /**
-     * 
-     * @param coupon
-     * @return
-     */
-    public Bond setCoupon(BigDecimal coupon){
-        this.coupon = coupon;
-        return this; 
-    }
-
-    /**
-     * 
-     * @param bookValue
-     * @return
-     */
-    public Bond setBookValue(BigDecimal bookValue){
-        this.bookValue = bookValue;
-        return this;
-    }
-
-    /**
-     * 
-     * @param fairValue
-     * @return
-     */
-    public Bond setFairValue(BigDecimal fairValue){
-        this.fairValue = fairValue;
-        return this;
-    }
-
-    /**
-     * 
-     * @param amount
-     * @return
-     */
-    public Bond setAmount(BigDecimal amount){
-        this.amount = amount;
-        return this;
-    }
-    /**
-     * 
-     * @param deleteFlag
-     * @return
-     */
-    public Bond setDeleteFlag(boolean deleteFlag){
-        this.deleteFlag = deleteFlag;
-        return this;
-    }
-
-    public String getCode(){
-        return this.code;
-    }
-
-    public String getName(){
-        return this.name;
-    }
-
-    public String getMaturity(){
-        return this.maturity;
-    }
-
-    public BigDecimal getCoupon(){
-        return this.coupon;
-    }
-
-    public BigDecimal getFairValue(){
-        return this.fairValue;
-    }
-
-    public BigDecimal getBookValue(){
-        return this.bookValue;
-    }
-
-    public BigDecimal getAmount(){
-        return this.amount;
-    }
-
-    public boolean getDeleteFlag(){
-        return this.deleteFlag;
-    }
 
 }
