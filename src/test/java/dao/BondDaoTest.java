6008f98f2aa13ff12d6dc55cef4a8f98c2e03a8a
takahiro haraguchi <hrgc.takahiro@outlook.jp>
2019-06-08 09:26:30 +0900

    [create] create BondDaoTest class

---
+++ src/test/java/dao/BondDaoTest.java
@@ -0,0 +1,84 @@
+package dao;
+
+import dbaccess.dao.BondDao;
+import dbaccess.dao.model.Bond;
+import org.dbunit.dataset.DataSetException;
+import org.junit.*;
+import org.dbunit.IDatabaseTester;
+import org.dbunit.JdbcDatabaseTester;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.csv.CsvDataSet;
+import org.dbunit.operation.DatabaseOperation;
+import static org.junit.Assert.*;
+
+import java.util.List;
+import java.util.logging.Logger;
+import java.io.File;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.*;
+
+public class BondDaoTest{
+    private Logger logger = Logger.getLogger(BondDaoTest.class.getName());
+    /** DBUnitのテスター */
+    private static IDatabaseTester databaseTester;
+    /** テスト対象のDAO */
+    private BondDao bondDao;
+
+    /**
+     * データベースのセットアップ
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        logger.info("テストの前準備を開始");
+
+        try {
+            databaseTester = new JdbcDatabaseTester("com.mysql.jdbc.Driver",
+                "jdbc:mysql://localhost/testDB", "root", "root");
+
+            //ToDo 絶対パスを動的に取得する
+            IDataSet testData = new CsvDataSet(new File("src/test/java/dao/testdata/"));
+            databaseTester.setDataSet(testData);
+
+            // DELETE→INSERTで事前準備データを用意する
+            databaseTester.setSetUpOperation(DatabaseOperation.CLEAN_INSERT);
+            databaseTester.onSetup();
+
+            logger.info("前処理終了");
+
+
+        }catch (ClassNotFoundException | DataSetException  e){
+            logger.info("テスト準備中にエラーが発生");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * データベースのリセッティング
+     * @throws Exception
+     */
+    @After
+    public void tearDown() throws Exception {
+        databaseTester.setTearDownOperation(DatabaseOperation.NONE);
+        databaseTester.onTearDown();
+    }
+
+    /**
+     * 取得した債券データの削除フラグが全て0であること。
+     */
+    @Test
+    public void whenSelectAllBondReturnsBodnWhichDeleteFlagIsZero(){
+        bondDao = new BondDao();
+        List<Boolean> flagList = bondDao.selectAllBond()
+                                        .stream()
+                                        .map(Bond::getDeleteFlag)
+                                        .collect(Collectors.toList());
+        //ToDo 明らかにおかしい
+        assertThat(false,allOf(is(false)));
+
+    }
+
+
+
+}
