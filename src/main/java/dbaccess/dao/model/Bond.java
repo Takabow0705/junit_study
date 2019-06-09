18b8e9396041d174eb1e95fd4565e7370ebbe405
takahiro haraguchi <hrgc.takahiro@outlook.jp>
2019-06-09 20:53:33 +0900

    [remove] remove Getter Setter annotation

--- src/main/java/dbaccess/dao/model/Bond.java
+++ src/main/java/dbaccess/dao/model/Bond.java
@@ -9,36 +9,20 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 public class Bond {
     /* 債券コード**/
-    @Getter
-    @Setter
     private String code;
     /* 債券名称**/
-    @Getter
-    @Setter
     private String name;
     /* 債券の償還年限**/
-    @Getter
-    @Setter
     private String maturity;
     /* クーポンレート**/
-    @Getter
-    @Setter
     private BigDecimal coupon;
     /* 取得簿価**/
-    @Getter
-    @Setter
     private BigDecimal bookValue;
     /* 時価**/
-    @Getter
-    @Setter
     private BigDecimal fairValue;
     /* 債券数量**/
-    @Getter
-    @Setter
     private BigDecimal amount;
     /* データの論理削除用フラグ**/
-    @Getter
-    @Setter
     private Boolean deleteFlag;
 
     /**
