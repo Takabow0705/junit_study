3a89996357729a0dee26b51f23e20fd18cf7de02
takahiro haraguchi <hrgc.takahiro@outlook.jp>
2019-06-09 20:57:47 +0900

    [revise] add NonNull

--- src/main/java/dbaccess/dao/model/Bond.java
+++ src/main/java/dbaccess/dao/model/Bond.java
@@ -9,20 +9,27 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 public class Bond {
     /* 債券コード**/
+    @NonNull
     private String code;
     /* 債券名称**/
+    @NonNull
     private String name;
     /* 債券の償還年限**/
+    @NonNull
     private String maturity;
     /* クーポンレート**/
+    @NonNull
     private BigDecimal coupon;
     /* 取得簿価**/
+    @NonNull
     private BigDecimal bookValue;
     /* 時価**/
     private BigDecimal fairValue;
     /* 債券数量**/
+    @NonNull
     private BigDecimal amount;
     /* データの論理削除用フラグ**/
+    @NonNull
     private Boolean deleteFlag;
 
     /**
