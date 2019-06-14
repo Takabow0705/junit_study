d82fad4f90280bff3b2ed27cb076ac699d9b95b1
takahiro-haraguchi <hrgc.takahir@outlook.jp>
2019-06-15 02:00:40 +0900

    [create] create ModelTests Suites

---
+++ src/test/java/dao/ModelTests.java
@@ -0,0 +1,12 @@
+package dao;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Model全般のテストを集約するクラス
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({BondTest.class})
+public class ModelTests {
+}
